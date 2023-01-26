359a5d0cb0b79c321e261ca354a02cae11b952f4
zayka <120934899+zayka-12@users.noreply.github.com>
2023-01-27 00:01:52 +0300

    comment

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 4 * : Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. Не использовать строки для расчета.
+
+int DeleteSecondDigit(int number)
+{
+    int a = number / 100;
+    int b = number % 10;
+    int result = a * 10 + b;
+    return result;
+}
+
+// int gen = 0;
+// while(gen < 101)
+// {
+int i = new Random().Next(100, 1000);
+Console.WriteLine($"Сгенерировано число: {i}");
+Console.WriteLine($"Новое число без второй цифры: {DeleteSecondDigit(i)}");
